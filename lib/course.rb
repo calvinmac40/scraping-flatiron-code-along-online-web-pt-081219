61ab600bc736e9fb15823ec1843d323f8540f927
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-12 15:23:48 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,36 @@
-
+class Course
+  @@all = []
+  
+  def initialize
+    self.class.all << self
+  end
+  
+  def title=(title)
+    @title = title
+  end
+  
+  def title 
+    @title
+  end
+  
+  def schedule=(schedule)
+    @schedule = schedule
+  end
+  
+  def schedule
+    @schedule
+  end
+  
+  def description=(description)
+    @description = description
+  end
+  
+  def description
+    @description
+  end
+  
+  def self.all
+    @@all 
+  end
+end
 
