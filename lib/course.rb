5ff8187fb3a496339e907ab995ca710d13cd60ec
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-12 19:53:28 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,43 @@
-
+class Course
+  
+  attr_accessor :title, :schedule, :description
+  
+  @@all = []
+  
+  def initialize
+    self.class.all << self
+  end
+  
+  def title=(title)
+    @title = title
+  end
+  
+  def title 
+    @title
+  end
+  
+  def schedule=(schedule)
+    @schedule = schedule
+  end
+  
+  def schedule
+    @schedule
+  end
+  
+  def description=(description)
+    @description = description
+  end
+  
+  def description
+    @description
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+end
 
